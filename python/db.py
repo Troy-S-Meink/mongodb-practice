ee7079a155dca08bb086f6eefe4aa42f1d8a3f84
Troy-S-Meink <ked6na@virginia.edu>
2025-03-28 01:19:10 +0000

    fixed stuff

--- python/db.py
+++ python/db.py
@@ -3,9 +3,9 @@ from bson.json_util import dumps
 import os
 
 # mongohost should look something like 'mongodb+srv://cluster0.xxxxx.mongodb.net/'
-MONGOUSER = os.getenv('MONGOUSER')
-MONGOPASS = os.getenv('MONGOPASS')
-MONGOHOST = os.getenv('MONGOHOST')
+MONGOUSER = os.getenv('USER')
+MONGOPASS = os.getenv('PASS')
+MONGOHOST = os.getenv('HOST')
 client = MongoClient(MONGOHOST, username=MONGOUSER, password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 sampler = client.sample_restaurants
 restaurants = sampler.restaurants
